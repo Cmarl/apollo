2dd6a246b74ac79412d7f0358aa2f4a59c7657f7
Clyde G Marl <marl.clyde@gmail.com>
2015-04-09 22:52:10 -0700

    init function, player on click

--- client/index.js
+++ client/index.js
@@ -1,1 +1,15 @@
 'use strict';
+
+$(document).ready(init);
+
+var players = {red: 'pRed'}
+var players = {black: 'pBlack'}
+
+function init(){
+  $('.pBlack').click(select);
+  $('.pRed').click(select);
+}
+
+function select(){
+  console.log($(this).data());
+}
